0559d00660a27973533633c87f5fb600346cbc84
Paul Hoadley <paulh@logicsquad.net>
2025-06-06 20:22:54 +0930

    Changes next() and handles em-dash as special case.

--- src/main/java/net/logicsquad/ibis/Tokenizer.java
+++ src/main/java/net/logicsquad/ibis/Tokenizer.java
@@ -1,6 +1,10 @@
 package net.logicsquad.ibis;
 
 import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Tokenizer {
 	private final BreakIterator breakIterator = BreakIterator.getWordInstance();
@@ -10,6 +14,8 @@ public class Tokenizer {
 	private int start;
 
 	private int end;
+
+	private Queue<Word> queue = new LinkedList<>(); 
 
 	public Tokenizer(String text) {
 		this.text = text;
@@ -30,11 +36,20 @@ public class Tokenizer {
 		return word;
 	}
 
-	public String next() {
+	public Word next() {
+		if (!queue.isEmpty()) {
+			return queue.remove();
+		}
 		String word = candidateNext();
 		while (!Character.isLetterOrDigit(word.charAt(0))) {
 			word = candidateNext();
 		}
-		return word;
+		if (word.contains("—")) {
+			int i = word.indexOf('—');
+			queue.add(new Word(word.substring(i + 1), i + 1, end));
+			return new Word(word.substring(0, i), start, i);
+		} else {
+			return new Word(word, start, end);
+		}
 	}
 }
